237386300ddf16d3b428e1f80f0a6f6bc73985cd
tseijp <_@tsei.jp>
2024-11-10 18:18:47 +0900

    chore: wip

--- App.tsx
+++ App.tsx
@@ -1,15 +1,45 @@
 import coordinate from "./munsell-coordinate";
 import interpolate from "./munsell-interpolate";
-import { Canvas, ThreeEvent } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import { useMemo } from "react";
+import { Canvas, GroupProps, ThreeEvent, useFrame } from "@react-three/fiber";
+import { PresentationControls } from "@react-three/drei";
+import { useMemo, useRef, useSyncExternalStore } from "react";
 import * as THREE from "three";
+
+function range(n = 0) {
+  const ret = new Array(n);
+  for (; n--; ) ret[n] = n;
+  return ret;
+}
+
+const I = 40; // hue
+const J = 15; // value
+const K = 13; // 26; // chroma
+
+let ijk = { i: 10, j: 11, k: 4 };
+
+const listeners = new Set<Function>();
+
+const set = ({ i = ijk.i, j = ijk.j, k = ijk.k }) => {
+  ijk = { i, j, k };
+  listeners.forEach((f) => f());
+};
+
+const sub = (update = () => {}) => {
+  listeners.add(update);
+  return () => {
+    listeners.delete(update);
+  };
+};
+
+const get = () => ijk;
+
+const useIJK = () => useSyncExternalStore(sub, get, get);
 
 function createInstances() {
   const ret = [];
-  for (let i = 0; i < 40; i++) {
-    for (let j = 0; j < 15; j++) {
-      for (let k = 0; k < 26; k++) {
+  for (let i = 0; i < I; i++) {
+    for (let j = 0; j < J; j++) {
+      for (let k = 0; k < K; k++) {
         const color = interpolate(i, j, k);
         if (color.some(isNaN)) continue;
         const coord = coordinate(i, j, k);
@@ -93,6 +123,7 @@ function createStore(instances: Instances) {
     pointer = createPointer();
     document.body.appendChild(pointer);
     window.addEventListener("mousemove", move);
+    window.addEventListener("mouseleave", leave);
     instances.forEach(init);
     mesh.instanceMatrix.needsUpdate = true;
     if (mesh.instanceColor) mesh.instanceColor.needsUpdate = true;
@@ -121,20 +152,116 @@ function Instances() {
     <instancedMesh
       ref={store.ref}
       onPointerEnter={store.enter}
+      onPointerLeave={store.leave}
       args={[geometry, material, instances.length]}
     />
   );
 }
 
+const PI = 3.14;
+const _I = (2 * PI) / I;
+
+function Ring() {
+  const ijk = useIJK();
+  return range(K).map((k) =>
+    range(I).map((i) => {
+      const color = interpolate(i, ijk.j, k);
+      const disable = isNaN(color[0]) || isNaN(color[1]) || isNaN(color[2]);
+      if (disable) return null;
+      return (
+        <mesh key={i} onClick={() => set({ i })}>
+          <ringGeometry
+            args={[k - 0.5, k + 0.5, 30, 8, _I * (I - i - 1), _I - 0.01]}
+          />
+          <meshBasicMaterial color={color} />
+        </mesh>
+      );
+    })
+  );
+}
+
+const width = `calc(min(${(100 / J) << 0}vw, ${(100 / K) << 0}vh))`;
+const height = width;
+
+function Palette() {
+  const ijk = useIJK();
+  return range(K).map((_k) =>
+    range(J).map((j) => {
+      const k = K - _k - 1;
+      const key = `${j}-${k}`;
+      const color = interpolate(ijk.i, j, k);
+      const disable = isNaN(color[0]) || isNaN(color[1]) || isNaN(color[2]);
+      const background = cssrgb(color);
+      return (
+        <div
+          key={key}
+          style={
+            disable ? {} : { width, height, background, pointerEvents: "auto" }
+          }
+          onClick={() => set({ j, k: K - k - 1 })}
+        />
+      );
+    })
+  );
+}
+
+function RotateGroup(props: GroupProps) {
+  const ref = useRef<THREE.Group>(null!);
+  useFrame(() => {
+    ref.current.rotation.z += 0.0005;
+  });
+  return <group ref={ref} {...props} />;
+}
+
 function App() {
+  const aspect = window.innerWidth / window.innerHeight;
   return (
-    <Canvas style={{ position: "fixed", top: 0, left: 0 }} linear flat legacy>
-      <color attach="background" args={["#A1A1A1"]} />
-      <group position-y={-4}>
-        <Instances />
-      </group>
-      <OrbitControls />
-    </Canvas>
+    <>
+      <Canvas
+        style={{ position: "fixed", top: 0, left: 0 }}
+        linear
+        flat
+        legacy
+        orthographic
+        camera={{
+          position: [0, 0, 10],
+          zoom: 2,
+          top: K / aspect,
+          bottom: -K / aspect,
+          left: -K,
+          right: K,
+          near: 1,
+          far: 200,
+        }}
+      >
+        <color attach="background" args={["#A1A1A1"]} />
+        <PresentationControls snap global polar={[-PI, PI]}>
+          <RotateGroup>
+            <group position-z={-J / 2 - 1} rotation-x={PI / 2}>
+              <Instances />
+            </group>
+            <group position-z={-J / 2}>
+              <Ring />
+            </group>
+          </RotateGroup>
+        </PresentationControls>
+      </Canvas>
+      <div
+        style={{
+          bottom: 0,
+          gap: `calc(${width} * 0.1)`,
+          left: "50%",
+          transform: "translateX(-50%)",
+          position: "fixed",
+          display: "grid",
+          justifyContent: "center",
+          pointerEvents: "none",
+          gridTemplateColumns: `repeat(${J}, 1fr)`,
+        }}
+      >
+        <Palette />
+      </div>
+    </>
   );
 }
 
--- munsell-coordinate.ts
+++ munsell-coordinate.ts
@@ -2,14 +2,13 @@ type Vec3 = [x: number, y: number, z: number];
 
 export default function coordinate(i = 0, j = 0, k = 0) {
   const phi = (2 * Math.PI * i) / 40;
-  const r = 0.5;
-  const x = Math.cos(phi) * k * r;
-  const y = j * 0.5;
-  const z = Math.sin(phi) * k * r;
+  const x = Math.cos(phi) * k;
+  const y = j;
+  const z = Math.sin(phi) * k;
   const ry = phi;
   return {
     pos: [x, y, z] as Vec3,
     rot: [0, -ry, 0] as Vec3,
-    scale: [0.5, 0.5, 0.1] as Vec3,
+    scale: [1, 1, 0.1] as Vec3,
   };
 }
